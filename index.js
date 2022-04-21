e3b8b2a56b5b40c727a0865b4aa167c24360047b
Diachao <3430737927@qq.com>
2022-04-21 23:31:41 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-21 23:27:55
+ * @LastEditTime: 2022-04-21 23:28:00
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vscode_xcx\index.js
+ */
